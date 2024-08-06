99a960b182b2503569af4363f8a956164c5dacb0
Anuki <anuki.thiyara@gmail.com>
2024-08-06 07:20:29 +0530

    Small change made to the 3rd answer

--- 7_3.scala
+++ 7_3.scala
@@ -1,10 +1,4 @@
 object PrimeFilter {
-  def main(args: Array[String]): Unit = {
-    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val primes = filterPrime(numbers)
-    println(primes)
-  }
-
   def filterPrime(numbers: List[Int]): List[Int] = {
     val isPrime: Int => Boolean = (num: Int) => {
       if (num <= 1) false
@@ -12,5 +6,11 @@ object PrimeFilter {
       else !(2 to Math.sqrt(num).toInt).exists(x => num % x == 0)
     }
     numbers.filter(isPrime)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val primes = filterPrime(numbers)
+    println(primes)
   }
 }
